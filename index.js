f5ca2f81c49a4f25c4c60e2630237968247d5d65
shraddha-gawde <shraddha.gawde1999@gmail.com>
2024-03-11 17:32:02 +0530

    swagger doc creation started

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
 const express = require("express")
 const { connection } = require("./db")
+
+const swaggerJSDoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
+
 const { userRouter } = require("./routes/user.routes")
 const { flightRouter } = require("./routes/flight.routes")
 const { bookingRouter } = require("./routes/booking.routes")
 const app = express()
 
 app.use(express.json())
+
+const options = {
+    definition: {
+      openapi: "3.0.0",
+      info: {
+        title: "Air-ticket-Booking",
+        version: "1.0.0",
+      },
+      servers: [
+        {
+          url: "http://localhost:4040",
+        },
+        {
+          url: "https://air-ticket-booking-uxxg.onrender.com",
+        },
+      ],
+    },
+    apis: ["./routes/*.js"],
+  };
+  
+  const openApiSpec = swaggerJSDoc(options);
+  
+  //Building complete UI
+  app.use("/apidocs", swaggerUi.serve, swaggerUi.setup(openApiSpec));
 
 app.get('/',(req,res)=>{
     res.json("this is home")
